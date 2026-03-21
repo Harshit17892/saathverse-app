4b7a71d37707b245dcd5e10b4a63466de77594ea
Harshit Verma <harshit.21scse1180220@galgotiasuniversity.edu.in>
2026-03-21 23:01:40 +0530

    Remove GamificationWidget from Discover component
    Removed GamificationWidget component from Discover page.

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -4,7 +4,6 @@ import ShowcaseCarousel from "@/components/ShowcaseCarousel";
 import FeaturesSection from "@/components/FeaturesSection";
 import DiscoverHub from "@/components/discover/DiscoverHub";
 import SpotlightCarousel from "@/components/discover/SpotlightCarousel";
-import GamificationWidget from "@/components/discover/GamificationWidget";
 import Footer from "@/components/Footer";
 
 const Discover = () => {
@@ -12,7 +11,6 @@ const Discover = () => {
     <div className="min-h-screen bg-background pt-20">
       <Navbar />
       <DiscoverHeroBanner />
-      <GamificationWidget />
       <ShowcaseCarousel />
       <FeaturesSection />
       <DiscoverHub />
